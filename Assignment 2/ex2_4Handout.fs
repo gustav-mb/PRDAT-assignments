68f6c9e339d25366c3687946d2567ce6d8b0be85
Gustav Metnik-Beck <gustav.mb@outlook.com>
2022-09-10 12:45:29 +0200

    feat: Completed Exercise 2.4 and 2.5
    Co-authored-by: alysondsouza <alysondsouza@users.noreply.github.com>
    Co-authored-by: Ahmed Bassam Galal <ShehabSherbiny@users.noreply.github.com>

--- Assignment 2/ex2_4Handout.fs
+++ Assignment 2/ex2_4Handout.fs
@@ -1,9 +1,10 @@
 open Expr
-open Absyn 
+open Absyn
 
 (* Ex 2.4 - assemble to integers *)
 (* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
 
+// Exercise PLC 2.4
 let sinstrToInt = function
   | SCstI i -> [0;i]
   | SVar i  -> [1;i]
@@ -14,16 +15,18 @@ let sinstrToInt = function
   | SSwap   -> [6];;
 
 let assemble instrs = 
-  List.fold (fun acc x -> sinstrToInt x @ acc ) [] instrs;;
+  List.fold (fun acc x -> acc  @ sinstrToInt x) [] instrs;;
 
 (* Output the integers in list inss to the text file called fname: *)
 
+// Exercise PLC 2.5
 let intsToFile (inss : int list) (fname : string) = 
     let text = String.concat " " (List.map string inss)
     System.IO.File.WriteAllText(fname, text);;
 
 let e1 = Let("z", CstI 17, Prim("+",Var "z", Var "z"));;
 
-scomp e1 [];; //val it : sinstr list = [SCstI 17; SVar 0; SVar 1; SAdd; SSwap; SPop]
+let e1Compiled = scomp e1 [];; //val it : sinstr list = [SCstI 17; SVar 0; SVar 1; SAdd; SSwap; SPop]
 
-assemble (scomp e1 []);; //val it : int list = [0; 17; 1; 0; 1; 1; 2; 6; 5]
+let test1 = assemble (scomp e1 []);; //val it : int list = [0; 17; 1; 0; 1; 1; 2; 6; 5]
+intsToFile test1 "test"
