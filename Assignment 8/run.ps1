4d29c51db0d7e02993da0883763849dd9e311ed1
Gustav Metnik-Beck <gustav.mb@outlook.com>
2022-11-06 11:45:36 +0100

    feat(run.ps1): Created script for compiling 9.1 and 9.2

--- Assignment 8/run.ps1
+++ Assignment 8/run.ps1
@@ -1,1 +1,63 @@
-dotnet fsi -r .\bin\Debug\net6.0\FsLexYacc.Runtime.dll .\MicroC\Absyn.fs
+$selsortPath = ".\9.1\Selsort"
+
+function Main {
+    # Selsort
+    Write-Host "SETTING UP SELSORT" -BackgroundColor Green -ForegroundColor White
+    Write-Host
+    csc.exe /o .\Virtual\Selsort.cs -out:9.1/Selsort.exe
+    javac -d .\9.1 .\Virtual\Selsort.java
+
+    # Create bytecode files
+    CreateFiles $selsortPath
+
+    # StringConcatSpeed
+    Write-Host "STRING CONCAT SPEED" -BackgroundColor Green -ForegroundColor White
+    Write-Host
+    csc.exe /o .\Virtual\StringConcatSpeed.cs -out:9.2/StringConcatSpeed.exe
+}
+
+function CreateFiles($path) {
+    $il = [System.IO.File]::Exists($path + ".il")
+    $jvm = [System.IO.File]::Exists($path + ".jvmbytecode")
+    
+    if ($il) {
+        PromptUser -name ($path + ".il") $path
+    } else {
+        Il -name ($path + ".il") -path $path
+    }
+
+    if ($jvm) {
+        PromptUser -name ($path + ".jvmbytecode") $path
+    } else {
+        JVMByteCode -name ($path + ".jvmbytecode") -path $path
+    }
+}
+
+function PromptUser($name, $path) {
+    $answer = Read-Host -Prompt "$name has already been created. Override? (y/n)"
+    
+    if (($answer.ToLower() -eq "y") -or ($answer.ToLower() -eq "yes")) {
+        Override $name $path
+    }
+}
+
+function Override([string]$name, [string]$path) {
+    if ($name.EndsWith(".il")) {
+        Write-Host "Overriding $name!`n" -BackgroundColor Red
+        Il $name $path
+    }
+    elseif ($name.EndsWith(".jvmbytecode")) {
+        Write-Host "Overriding $name!`n" -BackgroundColor Red
+        JVMByteCode $name $path
+    }
+}
+
+function Il([string]$name, [string]$path) {
+    ildasm.exe /text "$path.exe" > $name
+}
+
+function JVMByteCode([string]$name, [string]$path) {
+    javap -verbose -c "$path.class" > $name
+}
+
+Main
