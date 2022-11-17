4457d601d0df2117d02e02c05c7057a6b90b055a
Gustav Metnik-Beck <gustav.mb@outlook.com>
2022-11-17 20:39:51 +0100

    feat: Added compile option flag

--- Assignment 9/run.ps1
+++ Assignment 9/run.ps1
@@ -1,1 +1,9 @@
-dotnet fsi -r .\bin\Debug\net6.0\FsLexYacc.Runtime.dll .\ListC\Absyn.fs .\ListC\Machine.fs .\CPar.fsi .\CPar.fs .\CLex.fs .\ListC\Comp.fs .\ListC\Parse.fs .\ListC\ParseAndComp.fs 
+param([bool]$fsi=$false)
+
+if ($fsi) {
+    Write-Host "Starting fsi"
+    dotnet fsi -r .\bin\Debug\net6.0\FsLexYacc.Runtime.dll .\ListC\Absyn.fs .\ListC\Machine.fs .\CPar.fsi .\CPar.fs .\CLex.fs .\ListC\Comp.fs .\ListC\Parse.fs .\ListC\ParseAndComp.fs 
+} else {
+    Write-Host "Compiling listmachine.c"
+    gcc -Wall -g -o listmachine .\ListVM\ListVM\listmachine.c
+}
